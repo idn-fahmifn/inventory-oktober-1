0797bf06378e8a39a11e5c563cb3a7fa5c15d66c
idn-fahmifn <fahmifn.idn@gmail.com>
2025-10-15 11:24:38 +0700

    3.2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// group
+// route group admin
+Route::middleware(['auth', 'verified'])->prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+// route group user
+Route::middleware(['auth', 'verified'])->prefix('user')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard-user');
+    })->name('dashboard.user');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
