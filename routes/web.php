7f9a22fcfc3d168b2b4597b8f15fbc1874908175
idn-fahmifn <fahmifn.idn@gmail.com>
2025-10-15 14:59:07 +0700

    3.4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RuanganController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,9 @@ Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->group(functio
 
     Route::get('petugas', [ProfileController::class, 'index'])->name('petugas.index');
     Route::post('petugas', [ProfileController::class, 'store'])->name('petugas.store');
+
+    Route::get('ruangan', [RuanganController::class, 'index'])->name('ruangan.index');
+
 
     
 });
